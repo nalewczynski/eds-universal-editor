3fa5ceafa18035c6ce4d77e39e4376c6cfe68745
Daniel Malicki <daniel.malicki@pwc.com>
2025-02-06 13:48:05 +0100

    Quote Block js 2

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   console.log("Siema z bloku quote");
-  const [quoteWrapper] = block.children();
+  const [quoteWrapper] = block.children;
   console.log(quoteWrapper);
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
