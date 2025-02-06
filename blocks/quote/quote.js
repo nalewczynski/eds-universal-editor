e7378ead3acdff723644f47f9956d7fe4a99e137
Daniel Malicki <daniel.malicki@pwc.com>
2025-02-06 16:05:01 +0100

    Eslint

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,8 +1,6 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
-  console.log(quoteWrapper);
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceWith(blockquote);
-
 }
