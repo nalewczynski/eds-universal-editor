8bb410d115c67922507f7cc876abbcfa01584fd7
julia-jedryczka-pwc-com <julia.jedryczka@pwc.com>
2025-03-21 10:15:35 +0100

    experiment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,6 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
-import Utils from '../../scripts/utils/utils';
+import Utils from '../../scripts/utils';
 
 export default function decorate(block) {
   console.log('block', block);
